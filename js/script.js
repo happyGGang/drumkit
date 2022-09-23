23d7885f8fdcb2df14c0bec5632b2b4aed47c45b
joy.gang <joy.gang@dxworks.kr>
2022-09-23 16:11:05 +0900

    [update] 드럼 동작 구현

--- js/script.js
+++ js/script.js
@@ -9,16 +9,65 @@
     return document.querySelectorAll(target)
   }
 
+  const keys = Array.from(getAll('.key'))
+
   const soundsRoot = '/assets/sounds/'
   const drumSounds = [
-    { key: 81, sound: '고양이효과음1.wav' },
-    { key: 87, sound: '고양이효과음2.wav' },
-    { key: 69, sound: '고양이효과음3.wav' },
-    { key: 65, sound: '고양이효과음4.wav' },
-    { key: 83, sound: '고양이효과음5.wav' },
-    { key: 68, sound: '고양이효과음6.wav' },
-    { key: 90, sound: '고양이효과음7.wav' },
-    { key: 88, sound: '고양이효과음8.wav' },
-    { key: 67, sound: '고양이효과음9.wav' },
+    { key: 81, sound: 'clap.wav' },
+    { key: 87, sound: 'crash.wav' },
+    { key: 69, sound: 'hihat.wav' },
+    { key: 65, sound: 'kick.wav' },
+    { key: 83, sound: 'openhat.wav' },
+    { key: 68, sound: 'ride.wav' },
+    { key: 90, sound: 'shaker.wav' },
+    { key: 88, sound: 'snare.wav' },
+    { key: 67, sound: 'tom.wav' },
   ]
+
+  const getAudioElement = (index) => {
+    const audio = document.createElement('audio')
+    audio.dataset.key = drumSounds[index].key
+    audio.src = soundsRoot + drumSounds[index].sound
+    return audio
+  }
+
+  const onMouseDown = (e) => {
+    const keycode = e.target.getAttribute('data-key')
+    playSound(keycode)
+  }
+
+  const onKeyDown = (e) => {
+    const keycode = e.keyCode
+    playSound(keycode)
+  }
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName === 'transform') {
+      e.target.classList.remove('playing')
+    }
+  }
+
+
+  const playSound = (keycode) => {
+    const $audio = get(`audio[data-key="${keycode}"]`)
+    const $key = get(`div[data-key="${keycode}"]`)
+    if ($key && $audio) {
+      $key.classList.add('playing')
+      $audio.currentTime = 0
+      $audio.play()
+    }
+  }
+
+  const init = () => {
+    window.addEventListener('keydown', onKeyDown)
+    keys.forEach((key, index) => {
+      const audio = getAudioElement(index)
+      key.appendChild(audio)
+      key.dataset.key = drumSounds[index].key
+      key.addEventListener('click', onMouseDown)
+      key.addEventListener('transitionend', onTransitionEnd)
+    })
+  }
+
+  init()
 })()
